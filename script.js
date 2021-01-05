be799941041b5e5b01deefe8c4730e078d56ea8e
Jim <jimmontour@gmail.com>
2021-01-05 10:22:56 -0500

    Added validation on blur for each input

--- script.js
+++ script.js
@@ -7,45 +7,66 @@ const confirmPassword = document.querySelector('#confirm-password');
 const submitBtn = document.querySelector('#submit');
 
 // Event Delegation:
-email.addEventListener('input', () => {
-  if (email.validity.typeMismatch) {
-    email.classList.add('invalid');
-    email.setCustomValidity('This needs to be a valid email!');
-  } else {
-    email.setCustomValidity('');
-    email.classList.add('valid');
-    email.classList.remove('invalid');
-  }
+email.addEventListener('blur', () => {
+   if (!email.validity.valid) {
+      email.classList.add('invalid');
+      email.classList.remove('valid');
+      email.setCustomValidity('Please use a valid email.');
+
+   } else if (email.validity.valid) {
+      email.classList.add('valid');
+      email.classList.remove('invalid');
+      email.setCustomValidity('');
+   }
 });
 
-country.addEventListener('input', () => {
-  if (country.validity.typeMismatch) {
-    country.setCustomValidity('This must be a valid country!');
-  } else {
-    country.setCustomValidity('');
-  }
+country.addEventListener('blur', () => {
+   if (!country.validity.valid) {
+      country.classList.add('invalid');
+      country.classList.remove('valid');
+      country.setCustomValidity('Please use a valid country.');
+
+   } else if (country.validity.valid) {
+      country.classList.add('valid');
+      country.classList.remove('invalid');
+      country.setCustomValidity('');
+   }
 });
 
-zipcode.addEventListener('input', () => {
-  if (zipcode.validity.typeMismatch) {
-    zipcode.setCustomValidity('This must be a valid 5 digit number!');
-  } else {
-    zipcode.setCustomValidity('');
-  }
+zipcode.addEventListener('blur', () => {
+   if (!zipcode.validity.valid) {
+      zipcode.classList.add('invalid');
+      zipcode.classList.remove('valid');
+      zipcode.setCustomValidity('Please use a valid 5 digit zipcode.');
+
+   } else if (zipcode.validity.valid) {
+      zipcode.classList.add('valid');
+      zipcode.classList.remove('invalid');
+      zipcode.setCustomValidity('');
+   }
 });
 
-password.addEventListener('input', () => {
-  if (password.validity.typeMismatch) {
-    password.setCustomValidity('Password must be between 6-20 characters!');
-  } else {
-    password.setCustomValidity('');
-  }
+password.addEventListener('blur', () => {
+   if (!password.validity.valid) {
+      password.classList.add('invalid');
+      password.classList.remove('valid');
+      password.setCustomValidity('Please use a valid password.');
+
+   } else if (password.validity.valid) {
+      password.classList.add('valid');
+      password.classList.remove('invalid');
+      password.setCustomValidity('');
+   }
 });
 
-confirmPassword.addEventListener('input', () => {
+confirmPassword.addEventListener('blur', () => {
   if (confirmPassword.value !== password.value) {
     confirmPassword.setCustomValidity('Passwords must match!');
+     confirmPassword.classList.add('invalid');
+     confirmPassword.classList.remove('valid');
   } else {
-    confirmPassword.setCustomValidity('');
+     confirmPassword.classList.add('valid');
+     confirmPassword.classList.remove('invalid');
+     confirmPassword.setCustomValidity('');
   }
 });
