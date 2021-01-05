18a337395790a808e9282086fd9d0705bada7a5e
Jim <jimmontour@gmail.com>
2021-01-05 11:20:31 -0500

    Added logic to the error messages on blur event

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const zipcode = document.querySelector('#zipcode');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 const submitBtn = document.querySelector('#submit');
+const inputs = Array.from(document.querySelectorAll('input'));
+const errors = document.querySelectorAll('.error');
+const errorMessage = document.querySelector('.error-message');
+const errorList = document.querySelector('.error-list')
 
 // Event Delegation:
 email.addEventListener('blur', () => {
    if (!email.validity.valid) {
       email.classList.add('invalid');
       email.classList.remove('valid');
-      email.setCustomValidity('Please use a valid email.');
+      email.nextElementSibling.innerText = "Please use a valid email."
 
    } else if (email.validity.valid) {
       email.classList.add('valid');
@@ -24,7 +28,7 @@ country.addEventListener('blur', () => {
    if (!country.validity.valid) {
       country.classList.add('invalid');
       country.classList.remove('valid');
-      country.setCustomValidity('Please use a valid country.');
+      country.nextElementSibling.innerText = "Please use a valid country."
 
    } else if (country.validity.valid) {
       country.classList.add('valid');
@@ -37,7 +41,7 @@ zipcode.addEventListener('blur', () => {
    if (!zipcode.validity.valid) {
       zipcode.classList.add('invalid');
       zipcode.classList.remove('valid');
-      zipcode.setCustomValidity('Please use a valid 5 digit zipcode.');
+      zipcode.nextElementSibling.innerText = "Please use a valid zipcode."
 
    } else if (zipcode.validity.valid) {
       zipcode.classList.add('valid');
@@ -50,7 +54,7 @@ password.addEventListener('blur', () => {
    if (!password.validity.valid) {
       password.classList.add('invalid');
       password.classList.remove('valid');
-      password.setCustomValidity('Please use a valid password.');
+      password.nextElementSibling.innerText = "Please use a valid password."
 
    } else if (password.validity.valid) {
       password.classList.add('valid');
@@ -61,7 +65,7 @@ password.addEventListener('blur', () => {
 
 confirmPassword.addEventListener('blur', () => {
   if (confirmPassword.value !== password.value) {
-    confirmPassword.setCustomValidity('Passwords must match!');
+     confirmPassword.nextElementSibling.innerText = "Passwords must match."
      confirmPassword.classList.add('invalid');
      confirmPassword.classList.remove('valid');
   } else {
@@ -70,3 +74,12 @@ confirmPassword.addEventListener('blur', () => {
      confirmPassword.setCustomValidity('');
   }
 });
+
+submitBtn.addEventListener('click', e => {
+   console.log(inputs)
+   inputs.forEach(input => {
+      if (!input.validity.valid) {
+         e.preventDefault();
+      }
+   });
+})
