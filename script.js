8e06e8d8a694ce2a423bdb319423bb90a5e49114
Jim <jimmontour@gmail.com>
2021-01-04 19:17:48 -0500

    Added DOM Cache

---
+++ script.js
@@ -0,0 +1,9 @@
+// DOM Cache
+const form = document.querySelector('form')
+const inputs = document.querySelectorAll('input');
+const submitBtn = document.querySelector('#submit');
+
+form.addEventListener('submit', (e) => {
+
+   console.log('working')
+})
